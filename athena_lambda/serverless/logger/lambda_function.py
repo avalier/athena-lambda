ca2c543e524c21bc302ffe0ec9ad5a5d93aa52cc
Nick Hadfield <nick@hadfield.io>
2024-12-17 08:58:28 +0800

    Added args to Dockerfile

--- athena_lambda/serverless/logger/lambda_function.py
+++ athena_lambda/serverless/logger/lambda_function.py
@@ -1,5 +1,6 @@
 
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -7,5 +8,7 @@ logger = logging.getLogger(__name__)
 
 def handler(event, context):  # noqa
     logger.info("Resize...")
+    logger.info(f"APP_GIT_SHA: ${os.environ.get('APP_GIT_SHA')}")
+    logger.info(f"APP_VERSION: ${os.environ.get('APP_VERSION')}")
     logger.info("Event: %s", event)
     print(f"Event: {event}")
