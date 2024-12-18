b49851bfaa6638147cb0724c2d3207de82c45254
Nick Hadfield <nick@hadfield.io>
2024-12-18 11:57:02 +0800

    Implemented deploy

--- athena_lambda/serverless/resize/lambda_function.py
+++ athena_lambda/serverless/resize/lambda_function.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 def handler(event, context):  # noqa
-    logger.info("Logger...")
+    logger.info("Resize...")
+    logger.info(f"APP_GIT_SHA: ${os.environ.get('APP_GIT_SHA')}")
+    logger.info(f"APP_VERSION: ${os.environ.get('APP_VERSION')}")
     logger.info("Event: %s", event)
-    print(f"Event: {event}")
