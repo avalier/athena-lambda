ea3cf8cc3c1b98351b2984245ca834eeb57f2ba5
Nick Hadfield <nick@hadfield.io>
2024-12-19 11:18:37 +0800

    Second attempt

--- .bin/get-app-version.sh
+++ .bin/get-app-version.sh
@@ -9,9 +9,9 @@ else
     echo "GITHUB_REF: $GITHUB_REF"
 fi
 
-if [[ $GITHUB_REF == /refs/tags/release/* ]]
+if [[ $GITHUB_REF == refs/tags/release/* ]]
 then
-    export APP_VERSION=$(echo "$GITHUB_REF" | sed "s/\/refs\/tags\/release\//$secondString/")
+    export APP_VERSION=$(echo "$GITHUB_REF" | sed "s/refs\/tags\/release\//$secondString/")
 else
     export APP_VERSION=""
 fi
