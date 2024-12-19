2713db837b2d32e03649685a0836a62b7ecdad17
Nick Hadfield <nick@hadfield.io>
2024-12-19 11:05:34 +0800

    Conditional docker image labeling (by git tag)

---
+++ .bin/get-app-verion.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+if [ -z "$GITHUB_REF" ]
+then
+    echo "Missing ENV: GITHUB_REF" 
+    exit 1
+else
+    echo "GITHUB_REF: $GITHUB_REF"
+fi
+
+if [[ $GITHUB_REF == /refs/tags/release/* ]]
+then
+    export APP_VERSION=$(echo "$GITHUB_REF" | sed "s/\/refs\/tags\/release\//$secondString/")
+else
+    export APP_VERSION=""
+fi
